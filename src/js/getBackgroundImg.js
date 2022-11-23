09932f786307eee52a16e87c5f9180e2097a4106
Hussein Guevara <80933892+HusseinGuevara@users.noreply.github.com>
2022-11-23 14:05:50 -0800

    Add files via upload

--- src/js/getBackgroundImg.js
+++ src/js/getBackgroundImg.js
@@ -1,16 +1,33 @@
 'use strict';
 
+// HTML Elements
+const backgroundImg = document.querySelector('.top_content_wrapper');
+
 // Get background images behind the scenes while HTML continues to load
 
+const API_KEY = 'af9ee41cb42070ee6be415fb1d2ff113';
+const IMG_API = 'https://image.tmdb.org/t/p/w1280';
+const url = 'https://api.themoviedb.org/3/trending/movie/week?api_key='
 const getBackgroundImg = async function() {
-    const API_KEY = 'af9ee41cb42070ee6be415fb1d2ff113';
-    const url = 'https://api.themoviedb.org/3/trending/movie/week?api_key='
+   
     const resMovies = await fetch(`${url + API_KEY}`);
     const resMoviesObj = await resMovies.json();  
 
     const backdropPaths = resMoviesObj.results.map((movie) => {
-        console.log(movie.backdrop_path)
+        return movie.backdrop_path
     })
-    console.log(backdropPaths)
+    console.log(backdropPaths[2])
+    document.addEventListener('load', loadBackgroundImg(backdropPaths))
 }
 getBackgroundImg();
+
+// Load a random image into container after page has loaded
+const loadBackgroundImg = async function(array) {
+
+    // Randomize index from array
+    function random(arr) {
+        return arr[Math.floor(Math.random()*arr.length)]
+    }
+    backgroundImg.style.background = `linear-gradient(to right, rgba(3, 37, 65, .6) 0%, rgba(3, 37, 65, .7) 100%), url('${IMG_API + random(array)}')`
+    
+}
---
+++ src/js/popularMovies.js
@@ -0,0 +1,4 @@
+'use strict';
+
+
+const API_KEY = 'af9ee41cb42070ee6be415fb1d2ff113';
